e59ffd0128499f4a4e135a05115c0b2f865ec917
Rajarshi Mondal <54451186+rajarshidcoder@users.noreply.github.com>
2025-03-25 15:42:41 +0530

    Update Tour.jsx

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -162,7 +162,7 @@ const Tour = () => {
           text: "Freshers Anchor",
         },
         {
-          img: "/public/img/Enthuzea 2025/fest 2024/DSC_1419.JPG",
+          img: "/public/img/Enthuzea 2025/fest 2024/DSC_0777.JPG",
           text: "Dance Performance",
         },
       ],
